6d1c4a01842126659deb837ba5deb7d6f5101501
Blaine Price <1wbprice@gmail.com>
2019-10-29 13:49:55 -0400

    Reverse direction of edges

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use petgraph::{graphmap::GraphMap, Directed, Direction};
+use petgraph::{graphmap::GraphMap, Directed, Direction, dot::{Dot, Config}};
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone, PartialOrd, Ord, Hash)]
 enum Dishes {
@@ -16,6 +16,12 @@ enum Ingredients {
 enum Food {
     Dishes(Dishes),
     Ingredients(Ingredients),
+    Actions(Actions)
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone, PartialOrd, Ord, Hash)]
+enum Actions {
+    Cook
 }
 
 fn main() {
@@ -24,35 +30,43 @@ fn main() {
     deps.add_node(Food::Ingredients(Ingredients::HotDogBun));
     deps.add_node(Food::Ingredients(Ingredients::HotDogWeiner));
     deps.add_node(Food::Ingredients(Ingredients::HotDogWeinerCooked));
+    deps.add_node(Food::Actions(Actions::Cook));
 
     deps.add_edge(
-        Food::Dishes(Dishes::HotDog),
         Food::Ingredients(Ingredients::HotDogWeinerCooked),
+        Food::Dishes(Dishes::HotDog),
         1.,
     );
     deps.add_edge(
-        Food::Dishes(Dishes::HotDog),
         Food::Ingredients(Ingredients::HotDogBun),
+        Food::Dishes(Dishes::HotDog),
         1.,
     );
     deps.add_edge(
-        Food::Ingredients(Ingredients::HotDogWeinerCooked),
         Food::Ingredients(Ingredients::HotDogWeiner),
+        Food::Ingredients(Ingredients::HotDogWeinerCooked),
         1.,
     );
+    deps.add_edge(
+        Food::Actions(Actions::Cook),
+        Food::Ingredients(Ingredients::HotDogWeinerCooked),
+        1.,
+    );
+
+    println!("{:?}", Dot::with_config(&deps, &[Config::EdgeNoLabel]));
 
     println!("Ingredients needed to make a hot dog");
-    for node in deps.neighbors(Food::Dishes(Dishes::HotDog)) {
+    for node in deps.neighbors_directed(Food::Dishes(Dishes::HotDog), Direction::Incoming) {
         dbg!(node);
     }
     println!("");
     println!("These ingredients can make:");
     println!("Hot Dog Weiner can be used to make:");
-    for node in deps.neighbors_directed(Food::Ingredients(Ingredients::HotDogWeinerCooked), Direction::Incoming) {
+    for node in deps.neighbors_directed(Food::Ingredients(Ingredients::HotDogWeinerCooked), Direction::Outgoing) {
         dbg!(node);
     }
     println!("Hot Dog Bun can be used to make:");
-    for node in deps.neighbors_directed(Food::Ingredients(Ingredients::HotDogBun), Direction::Incoming) {
+    for node in deps.neighbors_directed(Food::Ingredients(Ingredients::HotDogBun), Direction::Outgoing) {
         dbg!(node);
     }
 }
